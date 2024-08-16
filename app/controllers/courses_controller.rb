1a342a97d8c37b9ec4cbd9741f5d2979ecea88bc
Jenny <jennywei0102@gmail.com>
2024-08-16 23:41:09 +0800

    Show a single course.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,8 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def showSingleCourse
+    @course = Course.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "courses#index"
 
   get "/courses", to: "courses#index"
+  get "/courses/:id", to: "courses#showSingleCourse"
 end

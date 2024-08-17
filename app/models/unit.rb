8b912121e5d894c5ef5301b6f309880707d023c5
Jenny <jennywei0102@gmail.com>
2024-08-17 15:59:35 +0800

    Update validation for unit and course.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,18 @@
 class Unit < ApplicationRecord
   belongs_to :course
+
+  validates :unit_name,
+            presence: true,
+            length: { maximum: 50 },
+            format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers, and whitespaces" }
+
+  validates :unit_description,
+            length: { maximum: 500 },
+            format: { with: /\A[a-zA-Z0-9\s.,!?]+\z/, message: "only allows letters, numbers, whitespaces, and punctuation" },
+            allow_blank: true
+
+  validates :unit_content,
+            presence: true,
+            length: { maximum: 500 },
+            format: { with: /\A[a-zA-Z0-9\s.,!?]+\z/, message: "only allows letters, numbers, whitespaces, and punctuation" }
 end

5ada07a1bfde9da79bbbb9b8ed4f8e4625cb1dbd
Jenny <jennywei0102@gmail.com>
2024-08-18 22:49:34 +0800

    Now I can add units under each course.

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -5,6 +5,13 @@ class UnitsController < ApplicationController
     redirect_to course_path(@course)
   end
 
+  def destroy
+    @course = Course.find(params[:course_id])
+    @unit = @course.units.find(params[:id])
+    @unit.destroy
+    redirect_to article_path(@course), status: :see_other
+  end
+
   private
     def unit_params
       params.require(:unit).permit(:unit_name, :unit_description, :unit_content, :position)

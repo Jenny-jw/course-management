e6e65cae50ef68165faeb9f15b0275cfed73b7c6
Jenny <jennywei0102@gmail.com>
2024-08-22 00:29:46 +0800

    Delete unit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "courses#index"
 
   resources :courses do
-    resources :units, only: [:create, :edit, :update, :destroy] do
+    resources :units do
       collection do
         post :sort
       end

cf196e960ca4efe6c31e302be59c944f803843fb
Jenny <jennywei0102@gmail.com>
2024-08-26 16:47:32 +0800

    Course view _form tests v0 done.

---
+++ spec/views/courses/_form.html.erb_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "courses/_form.html.erb", type: :view do
+  before do
+    @course = Course.new(
+      course_name: "Ruby on Rails",
+      teacher_name: "Jane Doe",
+      course_description: "Learn Rails from scratch"
+    )
+
+    render partial: "courses/form", locals: { course: @course }
+  end
+
+  it "renders the course name field" do
+    expect(rendered).to have_field('Course name', with: @course.course_name)
+  end
+
+  it "renders the teacher name field" do
+    expect(rendered).to have_field('Teacher name', with: @course.teacher_name)
+  end
+
+  it "renders the course description field" do
+    expect(rendered).to have_field('Course description', with: @course.course_description)
+  end
+
+  it "displays validation error messages for course_name" do
+    @course.errors.add(:course_name, "can't be blank")
+    render partial: "courses/form", locals: { course: @course }
+    expect(rendered).to have_content("can't be blank")
+  end
+
+  it "renders the submit button" do
+    expect(rendered).to have_button('Create Course') # or 'Update Course' depending on the context
+  end
+end
